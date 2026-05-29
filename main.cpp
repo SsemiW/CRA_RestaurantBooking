dd00b48bffcbac4432c2803815d7b976efa546cb
SsemiW <asdfhck@naver.com>
2026-05-29 14:08:22 +0900

    refactoring

--- main.cpp
+++ main.cpp
@@ -26,6 +26,8 @@ protected:
     {
         NOT_ON_THE_HOUR = getTime(2021, 3, 26, 9, 5);
         ON_THE_HOUR = getTime(2021, 3, 26, 9, 0);
+
+        bookingScheduler.setSmsSender(&testableSmsSender);
     }
 
 public:
@@ -36,6 +38,7 @@ public:
     const int CAPACITY_PER_HOUR = 3;
 
     BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
+    TestableSmsSender testableSmsSender;
 };
 
 TEST_F(BookingItem, 예약은정시에만가능하다정시가아닌경우예약불가) {
@@ -95,9 +98,7 @@ TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면
 
 TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
     //arrange
-    TestableSmsSender testableSmsSender;
     Schedule* schedule = new Schedule{ ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER };
-    bookingScheduler.setSmsSender(&testableSmsSender);
 
     //act
     bookingScheduler.addSchedule(schedule);
